56988b8e2f640c75b523c4558ea140a71adb2937
木木夕 <2315368861@qq.com>
2026-01-24 12:51:30 +0800

    更新缓存方式

--- src/dream_toolbox/cache/cache.py
+++ src/dream_toolbox/cache/cache.py
@@ -27,7 +27,7 @@ class CacheDB(BaseDB):
             return None
         return res_json[0]['output']
     
-    def update_cache(self,input_json,output_json,input_hash):
+    def update_cache(self,input_hash,input_json,output_json):
         self.db.execute(f'INSERT OR IGNORE INTO {self.table} (hash,input,output) VALUES (?,?,?)',(input_hash,input_json,output_json))
 
 
@@ -37,6 +37,39 @@ class Cache:
         self.cache_name = cache_name
         self.cache_db = CacheDB(self.cache_dir,self.cache_name)
     
+    def input_encode(self,inp):
+        inp_json = json.dumps(inp,ensure_ascii=False)
+        inp_hash = hashlib.md5(inp_json.encode('utf-8')).hexdigest()
+        inp_encoded = {
+            'hash': inp_hash,
+            'json': inp_json,
+        }
+        return inp_encoded
+
+    def output_encode(self,out):
+        out_encoded = json.dumps(out,ensure_ascii=False)
+        return out_encoded
+    
+    def output_decode(self,out_encoded):
+        out = json.loads(out_encoded)
+        return out
+
+    def find_cache(self,inp_encoded):
+        inp_hash = inp_encoded['hash']
+        out_encoded = self.cache_db.search_cache(inp_hash)
+
+        if out_encoded is None:
+            out = None
+        else:
+            out = self.output_decode(out_encoded)
+        return out
+    
+    def save_cache(self,inp_encoded,out):
+        inp_hash = inp_encoded['hash']
+        inp_json = inp_encoded['json']
+        out_encoded = self.output_encode(out)
+        self.cache_db.update_cache(inp_hash,inp_json,out_encoded)   
+
     def __call__(self,func):
         @wraps(func)
         def wrapper(*args,**kwargs):
@@ -47,25 +80,13 @@ class Cache:
             inp=bound_args.arguments.copy()
             inp.pop('self',None)
             inp.pop('cls',None)
-            inp_kwargs=inp.pop('kwargs',{})
-
-            use_cache=inp_kwargs.get('use_cache',True)
-            if not use_cache:
-                return func(*args,**kwargs)
             
-            inp_json = json.dumps(inp,ensure_ascii=False,indent=2)
-            inp_hash = hashlib.md5(inp_json.encode('utf-8')).hexdigest()
+            inp_encoded = self.input_encode(inp)
+            out = self.find_cache(inp_encoded)
 
-            out_json = self.cache_db.search_cache(inp_hash)
-
-            if out_json is not None:
-                out = json.loads(out_json)['output']
-            else:
-                out = {'output': func(*args,**kwargs)}
-                out_json = json.dumps(out,ensure_ascii=False,indent=2)
-                self.cache_db.update_cache(inp_json,out_json,inp_hash)
-                out = out['output']
-
+            if out is None:
+                out = func(*args,**kwargs)
+                self.save_cache(inp_encoded,out)
             return out
     
         return wrapper
