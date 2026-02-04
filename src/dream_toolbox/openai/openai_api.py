3d146410de96eb4e59607547a3524e06af4c2b17
木木夕 <2315368861@qq.com>
2026-02-04 20:46:07 +0800

    add model

--- src/dream_toolbox/openai/openai_api.py
+++ src/dream_toolbox/openai/openai_api.py
@@ -1,6 +1,76 @@
 from openai import OpenAI
 from ..cache import Cache
 
+class Model:
+    def __init__(self,
+                api_key,
+                base_url,
+                default_model_args = None,
+                use_cache = True,
+                cache_dir='cache/OpenAIAPI/',
+                cache_name='model_cache'
+                ):
+        
+        self.api_key = api_key
+        self.base_url = base_url
+
+        self.client = OpenAI(
+            api_key = self.api_key, 
+            base_url = self.base_url,
+        )
+        self.default_model_args = default_model_args if default_model_args is not None else {}
+
+        self.use_cache = use_cache
+        if self.use_cache:
+            self.cache = Cache(cache_dir,cache_name)
+
+    def generate(self,messages,model_args = None,**kwargs):
+        if model_args is None:
+            model_args = self.default_model_args
+
+        if self.use_cache:
+            input={'messages':messages,'model_args':model_args,'kwargs':kwargs}
+            inp_encoded = self.cache.input_encode(input)
+            response = self.cache.find_cache(inp_encoded)
+            if response is not None:
+                return response
+
+        response={
+            'status':None,
+            'content':None,
+            'usage':{},
+        }        
+        try:
+            completion = self.client.chat.completions.create(
+                **model_args,
+                messages=messages,
+            )
+            response['status'] = 1
+            response['content'] = completion.choices[0].message.content
+        except Exception as e:
+            response['status'] = 0
+            response['content'] = str(e)
+
+        try:
+            response['usage']['prompt_tokens']=completion.usage.prompt_tokens
+            response['usage']['completion_tokens']=completion.usage.completion_tokens
+            response['usage']['total_tokens']=completion.usage.total_tokens
+        except:
+            response['usage']['prompt_tokens']=None
+            response['usage']['completion_tokens']=None
+            response['usage']['total_tokens']=None
+
+        try:
+            response['usage']['cached_tokens']=completion.usage.prompt_tokens_details.cached_tokens
+        except:
+            response['usage']['cached_tokens']=None
+
+        if self.use_cache:
+            self.cache.save_cache(inp_encoded,response)
+
+        return response
+
+
 class LLM:
     def __init__(self,
                 api_key,
